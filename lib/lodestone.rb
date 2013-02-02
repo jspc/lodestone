e1d6a9f41ed8f8fb8d6475e84071fedaa9eed6a0
jspc <james@zero-internet.org.uk>
2013-02-02 23:23:20 +0000

    Set the things

--- lib/lodestone.rb
+++ lib/lodestone.rb
@@ -5,13 +5,13 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'grammars')
 require "treetop"
 require "polyglot"
 require "lodestone"
+require "fileutils"
 
 module Lodestone
   @ls = LodestoneParser.new
   @opts = Hash.new
 
   def self.__parse definition
-    #"define directory /sss with perms 777 it should be user jcondron and should be group systems"
     @ls.parse( definition.to_s ).content.each do |thing|
       next if thing.nil?
       @opts.merge!( thing[0].to_sym => thing[1] )
@@ -22,8 +22,18 @@ module Lodestone
   def self.apply definition
     definition_hash = self.__parse definition
 
-    puts "chown #{definition_hash[:user]} #{definition_hash[:path]}"
-    puts "chgrp #{definition_hash[:group]} #{definition_hash[:path]}"
-    puts "chmod #{definition_hash[:perms]} #{definition_hash[:path]}"
+    unless File.exists? definition_hash[:path]
+      return false, "#{definition_hash[:path]} does not exist"
+    end
+    
+    begin
+      FileUtils.chown definition_hash[:user], definition_hash[:group], definition_hash[:path]
+      FileUtils.chmod definition_hash[:perms].to_i(8), definition_hash[:path]
+    rescue Exception => e
+      return false, "Failed to set the perms on #{definition_hash[:path]} - #{e}"
+    end
+
+    return true, "Succesfully applied definition to #{definition_hash[:path]}"
+
   end
 end
