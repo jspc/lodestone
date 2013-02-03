77324b4a6aa67c5009e54ab46835938186725a0d
jspc <james@zero-internet.org.uk>
2013-02-03 13:06:50 +0000

    Ensure fresh data on each run through

--- lib/lodestone.rb
+++ lib/lodestone.rb
@@ -9,31 +9,37 @@ require "fileutils"
 
 module Lodestone
   @ls = LodestoneParser.new
-  @opts = Hash.new
 
   def self.__parse definition
+    opts = Hash.new
     @ls.parse( definition.to_s ).content.each do |thing|
       next if thing.nil?
-      @opts.merge!( thing[0].to_sym => thing[1] )
+      opts.merge!( thing[0].to_sym => thing[1] )
     end
-    @opts
+    opts
   end
 
   def self.apply definition
     definition_hash = self.__parse definition
+    path  = definition_hash[:path]  or return false, "No file or dir specified"
+    user  = definition_hash[:user]  or nil
+    group = definition_hash[:group] or nil
+    perms = definition_hash[:perms] or nil
 
-    unless File.exists? definition_hash[:path]
-      return false, "#{definition_hash[:path]} does not exist"
+    unless File.exists? path
+      return false, "#{path} does not exist"
     end
     
     begin
-      FileUtils.chown definition_hash[:user], definition_hash[:group], definition_hash[:path]
-      FileUtils.chmod definition_hash[:perms].to_i(8), definition_hash[:path]
+      FileUtils.chown user, group, path
+      if perms
+        FileUtils.chmod perms.to_i(8), path
+      end
     rescue Exception => e
-      return false, "Failed to set the perms on #{definition_hash[:path]} - #{e}"
+      return false, "Failed to set the perms on #{path} - #{e}"
     end
 
-    return true, "Succesfully applied definition to #{definition_hash[:path]}"
+    return true, "Succesfully applied definition to #{path}"
 
   end
 end
